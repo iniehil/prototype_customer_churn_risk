9ce46c4b829772d8a38e174d10dfa62fe546d3bc
Inga Hilbolling-Nienstedt <inga.nienstedt@gmail.com>
2026-09-14 15:58:16 +0200

    Update demo.py
    remove import matplotlib and seaborn

--- demo.py
+++ demo.py
@@ -10,8 +10,6 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Title of the app
 st.title("Who is likely to chrun - and when?")
